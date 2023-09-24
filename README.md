5a20ae1c71771a4779799bc4c58e08b43ffb78da
jc <wnwlcks123@gmail.com>
2023-09-24 13:56:13 +0900

    Next Auth 설정하기

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # 'use client'
 
-서버 컴포넌트에서는 useState와 같은 클라이언트 훅스를 사용할 수 없으니   
+서버 컴포넌트에서는 useState와 같은 클라이언트 훅스를 사용할 수 없으니  
 사용하고 싶을 때 'use client'를 작성한다.
+
+# Prisma ORM
+
+`$ npm install -D prisma` //프리즈마 설치  
+`$ npx prisma init ` // 프리즈마 이니셜라이즈  
+`$ npx prisma db push` // 데이터베이스와 싱크를 맞춰준다.
+
+`https://authjs.dev/reference/adapter/prisma` //
+
+# Next Auth
+
+`$ npm install next-auth @prisma/client @next-auth/prisma-adapter`
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,15 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
-const NavItem = ({mobile}: {mobile?: boolean}) => {
+const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
+
   return (
     <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href="/admin">Admin</Link></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><Link href="/user">User</Link></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signout</button></li>
-      <li className='py-2 text-center border-b-4 cursor-pointer'><button>Signin</button></li>
+      <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/admin">Admin</Link></li>
+      <li className="py-2 text-center border-b-4 cursor-pointer"><Link href="/user">User</Link></li>
+      {session?.user 
+        ? 
+        (<li className="py-2 text-center border-b-4 cursor-pointer"><button
+        onClick={() => signOut()}>Signout</button></li>) 
+        : 
+        (<li className="py-2 text-center border-b-4 cursor-pointer"><button 
+        onClick={() => signIn()}
+        >Signin</button></li>)}
     </ul>
-  )
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
