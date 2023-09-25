fa0b65ecaa8bf291feca68b5880bcfaf42ef366f
jc <wnwlcks123@gmail.com>
2023-09-25 21:39:05 +0900

    Favorite 기능 생성하기

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -1,3 +1,4 @@
+import useFavorite from '@/app/hooks/useFavorite';
 import { User } from '@prisma/client';
 import React from 'react'
 import {AiFillHeart, AiOutlineHeart} from 'react-icons/ai'
@@ -11,15 +12,24 @@ const HeartButton = ({
   productId,
   currentUser
 }: HeartButtonProps) => {
+
+  const { hasFavorite, toggleFavorite } = useFavorite({
+    productId,
+    currentUser
+  })
+
+
   return (
-    <div className='relative transition cursor-pointer hover:opacity-80'>
+    <div 
+      onClick={toggleFavorite}
+    className='relative transition cursor-pointer hover:opacity-80'>
       <AiOutlineHeart 
           size={28}
           className='fill-white absolute -top-[2px] -right-[2px]'
       />
       <AiFillHeart 
           size={24}
-          className={'fill-rose-500'}
+          className={hasFavorite ? 'fill-rose-500' : 'fill-neutral-500'}
       />
     </div>
   )
