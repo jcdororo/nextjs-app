b556f3480e714a06e9bdf86735c8ef29c4a594e6
jc <wnwlcks123@gmail.com>
2023-09-25 12:57:40 +0900

    getServerSession

---
+++ src/app/actions/getCurrentUser.ts
@@ -0,0 +1,28 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if(!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma?.user.findUnique({
+      where: {
+        email: session.user.email
+      }
+    })
+    return currentUser;
+
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+}
