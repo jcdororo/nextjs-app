5115b7daee2136136d629801ceecab97bdd6cc45
jc <wnwlcks123@gmail.com>
2023-09-25 21:04:48 +0900

    홈 페이지 UI 생성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
 import getProducts, { ProductsParams } from '../actions/getProducts'
+import Container from '@/components/Container';
+import EmptyState from '@/components/EmptyState';
+import ProductCard from '@/components/ProductCard';
+import getCurrentUser from '../actions/getCurrentUser';
+import FloatingButton from '@/components/FloatingButton';
 
 interface Homprops {
   searchParams: ProductsParams
@@ -8,13 +12,40 @@ interface Homprops {
 export default async function Home({searchParams}: Homprops) {
 
   const products = await getProducts(searchParams);
+  const currentUser = await getCurrentUser();
 
   console.log('products',products)
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      누구나 볼 수 있는 페이지 입니다.
-    </main>
+    <Container>
+      {/* Category */}
+
+      {
+        products?.data.length === 0
+        ?
+        <EmptyState />
+        :
+        <>
+          <div className='grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6'>
+          {products.data.map((product)=> (
+            <ProductCard 
+               currentUser={currentUser}
+               key={product.id}
+               data={product}
+            />
+
+          ))}
+          </div>
+        </>
+      }
+
+
+      <FloatingButton 
+        href="/products/upload"
+
+      >+</FloatingButton>
+
+    </Container>
   )
 }
