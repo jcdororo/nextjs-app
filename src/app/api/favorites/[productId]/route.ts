04ecc870b960e5eee805a4e39d9c8ad16c3269a4
jc <wnwlcks123@gmail.com>
2023-09-26 13:25:37 +0900

    Favorite Delete Route 생성

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -36,3 +36,34 @@ export async function POST(request: Request, { params } : { params: Params }) {
   return NextResponse.json(user)
 
 }
+
+export async function DELETE(request: Request, { params } : { params: Params }) {
+
+  const currentUser = await getCurrentUser();
+
+  if(!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { productId } = params;
+
+  if(!productId || typeof productId !== 'string') {
+    throw new Error(`Invalid ID`);
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter(id => id !== productId);
+
+  const user = await prisma?.user.update({
+    where: {
+      id: currentUser.id
+    },
+    data: {
+      favoriteIds: favoriteIds
+    }
+  })
+
+  return NextResponse.json(user)
+
+}
