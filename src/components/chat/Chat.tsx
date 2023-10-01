00af68859e482654c712ce89cf7e94092e3f10a1
jc <wnwlcks123@gmail.com>
2023-10-01 20:48:40 +0900

    ChatHeader 컴포넌트 생성

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import { TUserWithChat } from '@/types'
 import React from 'react'
 import Input from './Input';
+import ChatHeader from './ChatHeader';
 
 interface ChatProps {
   currentUser: TUserWithChat;
@@ -18,6 +19,10 @@ const Chat = ({
   setLayout
 }: ChatProps) => {
 
+  const conversation =
+    currentUser?.conversations.find((conversation) =>
+      conversation.users.find((user) => user.id === receiver.receiverId))
+
   if(!receiver.receiverName || !currentUser) {
     return <div className='w-full h-full'></div>
   }
@@ -25,7 +30,17 @@ const Chat = ({
   return (
     <div className='w-full'>
       <div>
-        {/* chat header */}
+        <ChatHeader 
+          setLatyout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={
+            conversation?.messages
+              .filter(message => message.receiverId === currentUser.id)
+              .slice(-1)[0]?.createdAt
+          }
+        
+        />
       </div>
 
       <div className='flex flex-col gap-8 p-4 overflow-hidden h-[calc(100vh_-_60px_-_70px_-_80px)]'>
