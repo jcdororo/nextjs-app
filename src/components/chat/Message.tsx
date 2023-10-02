2ea4466113757b6aa489d8cf718f8373ff2e33ff
jc <wnwlcks123@gmail.com>
2023-10-02 13:34:42 +0900

    채팅 메세지 생성 시 이미지 업로드

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Avatar from '../Avatar';
 import { fromNow } from '@/helpers/dayjs';
+import Image from 'next/image';
 
 
 interface MessageProps {
@@ -37,6 +38,19 @@ const Message = ({
             {fromNow(time)}
           </span>
         </div>
+
+        {messageImage && (
+          <div className='overflow-hidden rounded-md mx-[0.6rem] max-w-[80%]'>
+            <Image 
+              src={messageImage}
+              width={300}
+              height={300}
+              alt=''
+            />
+
+          </div>
+        )}
+
         {messageText && (
           <div
             className={
